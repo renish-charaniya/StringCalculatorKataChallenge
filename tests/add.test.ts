b936a88154e4f3ced542bdcaf1fb8c1262eabc09
renish9923@gmail.com <renish9923@gmail.com>
2025-01-01 12:13:27 +0530

    feat(sum): performs addition of comma seperated values

--- tests/add.test.ts
+++ tests/add.test.ts
@@ -1,15 +1,26 @@
-import { StringCalculator } from "../src/add";
+import { StringCalculator } from '../src/add';
 
-describe("String Calculator", () => {
-  test("Should return Zero(0) for empty string input", () => {
+describe('String Calculator', () => {
+  test('Should return Zero(0) for empty string input', () => {
     const calculator = new StringCalculator();
-    const result = calculator.add("");
+    const result = calculator.add('');
     expect(result).toBe(0);
   });
 
-  test("Should return number for a single input", () => {
+  test('Should return number for a single input', () => {
     const calculator = new StringCalculator();
-    const result = calculator.add("1");
-    expect(result).toBe(1);
+    expect(calculator.add('1')).toBe(1);
+    expect(calculator.add('26')).toBe(26);
+  });
+
+  test('Should return sum of two numbers', () => {
+    const calculator = new StringCalculator();
+    expect(calculator.add('5,15')).toBe(20);
+    expect(calculator.add('25,75')).toBe(100);
+  });
+
+  test('Should return sum of all the numbers', () => {
+    const calculator = new StringCalculator();
+    expect(calculator.add('100,33,44')).toBe(177);
   });
 });
